e9c8e1ee31cc423eb6b6d3ff8ff6d56a11241057
mirak2010 <mirakhmad0101@yahoo.com>
2024-12-28 10:30:07 +0500

    #

--- checkdatatype.py
+++ checkdatatype.py
@@ -1,7 +1,7 @@
 a=5
 print("type of a: ", type(a))
 
-b=5.5
+b=6.5
 print("type of b: ", type(b))
 
 c=True
@@ -9,3 +9,16 @@ print('type of c: ', type(c))
 
 d="codingal"
 print('type of d: ', type(d))
+
+
+a=str(a)
+print(a, type(a))
+
+b=int(b)
+print(b, type(b))
+
+c=str(c)
+print(c, type(c))
+
+num=int(input("Please enter any number: "))
+print(num, type(num))
