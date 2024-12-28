8b1cd6c697430c97e0b9bb60f4fa21612e4035ec
mirak2010 <mirakhmad0101@yahoo.com>
2024-12-28 10:51:30 +0500

    3

--- checkdatatype.py
+++ checkdatatype.py
@@ -22,3 +22,15 @@ print(c, type(c))
 
 num=int(input("Please enter any number: "))
 print(num, type(num))
+
+
+
+str1="Hello"
+str2="World"
+
+print("Joined form: ", str1+str2)
+print(len(str1+str2))
+
+print(str1[1:3:])
+print(str1[::-1])
+print(str2[1::])
